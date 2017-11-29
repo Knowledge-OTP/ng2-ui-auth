69d98c42d6be0daf793518d8808be943b04f9477
assafhp <assafshp@zahav.net.il>
2017-11-29 17:38:05 +0200

    de

--- dist/utils.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Ron on 17/12/2015.
- */
-export declare function assign(target: any, ...src: any[]): any;
-export declare function joinUrl(baseUrl: string, url: string): any;
-export declare function merge(obj1: object, obj2: object): any;
-export declare function camelCase(name: any): any;
